5d68e8c8c1683a43d262703df56b2cdf441cdba1
Rémi Bloch <remi.bloch@sai-erp.net>
2020-11-18 15:25:33 +0100

    Fixed pipeline registration based on latest api

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 var assert = require('assert');
 var Metalsmith = require('metalsmith');
 var lunr = require('..');
+var lunr_ = require('lunr');
 
 describe('metalsmith-lunr', function(){
   it('should add JSON index file to metadata', function(done){
@@ -8,7 +9,10 @@ describe('metalsmith-lunr', function(){
     metalsmith
       .use(lunr())
       .build(function(err, files){
-        if (err) return done(err);
+        if (err) {
+            console.log(err);
+            return done(err);
+        }
         assert.equal(typeof files['searchIndex.json'], 'object');
         assert.equal(typeof files['searchIndex.json'].contents, 'object');
         assert.equal(Object.keys(files).length, 4);
@@ -57,4 +61,54 @@ describe('metalsmith-lunr', function(){
         done();
       });
   });
+
+  it('should be able to add pipeline functions', function(done){
+    var metalsmith = Metalsmith('test/fixtures/basic');
+    metalsmith
+      .use(lunr({
+        fields: {title:1, contents: 1},
+        indexPath: 'index.json',
+        ref: 'filePath',
+        whitelist: ['position', 'testPipe'],
+        pipelineFunctions: {
+            'testPipe' : (token) => {
+                token.metadata['testPipe'] = token.toString().length;
+                return token;
+            }
+        }
+      }))
+      .build(function(err, files){
+        if (err) return done(err);
+        index = JSON.parse(files['index.json'].contents);
+        assert.equal(index.invertedIndex[0][1]['contents']['one.md']['testPipe'][0], 10);
+        done();
+      });
+  });
+
+
+  it('should be able to find metas from pipeline functions', function(done){
+    var metalsmith = Metalsmith('test/fixtures/basic');
+    metalsmith
+      .use(lunr({
+        fields: {contents: 1},
+        indexPath: 'index.json',
+        ref: 'filePath',
+        whitelist: ['testMeta'],
+        pipelineFunctions: {
+            'testMeta' : (token) => {
+                token.metadata['testMeta'] = token.toString().length;
+                return token;
+            }
+        }
+      }))
+      .build(function(err, files){
+        if (err) return done(err);
+        let indexContent = JSON.parse(files['index.json'].contents);
+        let loaded = lunr_.Index.load(indexContent);
+        let result = loaded.search('python');
+        assert.equal(result[0].matchData.metadata['python'].contents.testMeta, 6);
+        done();
+      });
+  });
+
 });
